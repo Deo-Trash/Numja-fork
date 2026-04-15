a8a255ea703137628d73ed51cf63ef684412481f
Loic GIRAUD <lgiraud.giraud@gmail.com>
2026-04-15 10:14:15 +0200

    je farm les commits

--- numja/src/test/java/fr/uga/numja/NdarrayTest.java
+++ numja/src/test/java/fr/uga/numja/NdarrayTest.java
@@ -7,5 +7,10 @@ import static org.junit.Assert.assertEquals;
 
 public class NdarrayTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateZerosEmptyNdarray1D(){
+        Ndarray.zeros(0);
+    }
+
 
 }
